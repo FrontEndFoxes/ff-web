e63f090d88feb26d42cca92656d45b49e7d412e9
Jen Looper <jen.looper@gmail.com>
2020-09-06 13:53:16 -0400

    svg instead of png looks sharper

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,9 @@
   <!--Hero-->
   <div>
     <div class="pt-24">
-      <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row">
         <!--Left Col-->
-        <div
-          class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left"
-        >
+        <div class="flex flex-col w-full md:w-2/5">
           <h1 class="my-4 text-5xl font-bold leading-tight">Welcome!</h1>
           <p class="leading-normal text-2xl mb-8">
             Front-End Foxes are people who identify as women and who want to learn front-end technologies to
@@ -23,7 +21,7 @@
         </div>
         <!--Right Col-->
         <div class="w-full md:w-3/5 py-6 text-center">
-          <img class="w-full md:w-4/5 z-50" src="@/assets/logo.png" />
+          <img class="w-full md:w-4/5 z-50" src="@/assets/logo.svg" />
         </div>
       </div>
     </div>
