c8d5937e2cd92fe6fe29b6bd7369155ec05ea422
Cody Barr <cody.barr@gmail.com>
2020-10-06 17:56:39 -0400

    feat: add 404 page
    Also made footer sticky ;)

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
-	<div id="app">
-		<Header />
-		<transition name="fade"><router-view /></transition>
-		<Footer />
-	</div>
+  <div id="app" class="flex flex-col h-screen min-h-screen">
+    <Header />
+    <main class="relative flex-grow">
+      <transition name="fade"><router-view /></transition>
+    </main>
+    <Footer />
+  </div>
 </template>
 <script>
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 
 export default {
-	name: 'App',
-	components: {
-		Header,
-		Footer,
-	},
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 };
 </script>
 
