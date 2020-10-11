d03df6d5d4f4ffb16b18453899963ff04321a335
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-10-11 16:38:12 -0400

    French and Portuguese translations: add i18n declarations

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,15 @@
 import { en } from './en'
 import { es } from './es'
+import { fr } from './fr'
+import { pt } from './pt'
 
 const defaultLocale = 'en'
 
 const translations = {
   en: en,
   es: es,
+  fr: fr,
+  pt: pt,
 }
 
 export const valuesI18n = {
