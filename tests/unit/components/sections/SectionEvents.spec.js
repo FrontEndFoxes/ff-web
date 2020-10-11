d68c14263b3a92e88834cd4c103ea83d1cba8f0a
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-10-11 17:42:41 -0400

    Change data to props

--- tests/unit/components/sections/SectionEvents.spec.js
+++ tests/unit/components/sections/SectionEvents.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 
 // Component & Data
-import events from '@/assets/data/events-home'
+import ffevents from '@/assets/data/events-home'
 import SectionEvents from '@/components/sections/SectionEvents.vue'
 
 describe('SectionEvents.vue', () => {
@@ -9,25 +9,16 @@ describe('SectionEvents.vue', () => {
 
     const mountFunction = () => {
         return mount(SectionEvents, {
-            props: {
-                ffevents: {
-                    en: [
-                        {
-                            "id": 1,
-                            "title": "Japan Meetup",
-                            "date": "2020-10-04T18:25:43.511Z",
-                            "startEvent": "4:00 PM",
-                            "finishEvent": "05:30 PM",
-                            "location": "Madrid",
-                            "description": "Lorem ipsum dolor sit amet, consecte adipisc elit, sed do eiusmod tempor.",
-                            "cta": "meetup",
-                            "active": true,
-                            "image": "https://images.unsplash.com/photo-1505373877841-8d25f7d46678?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80",
-                            "link": "#"
-                        }
-                    ],
-                    es: []
-                }
+            data() {
+                return {
+                    ffevents: ffevents,
+                };
+            },
+            computed: {
+                events() {
+                    const currentLocal = this.$i18n.locale;
+                    return this.ffevents[currentLocal];
+                },
             },
             global: {
                 mocks: {
@@ -46,6 +37,6 @@ describe('SectionEvents.vue', () => {
 
     it('should be mounted and have events English data', () => {
         expect(!!wrapper.vm).toBeTruthy()
-        expect(wrapper.vm.ffevents["en"]).toEqual(events["en"])
+        expect(wrapper.vm.ffevents["en"]).toEqual(ffevents["en"])
     })
 })
