0e247555e2431d77edfc9bcbde9477af7adfce26
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-10-11 16:37:20 -0400

    Add declaration about French and Portuguese translations

--- src/assets/data/events-home/index.js
+++ src/assets/data/events-home/index.js
@@ -1,9 +1,13 @@
 import en from './en-events-home.json'
 import es from './es-events-home.json'
+import fr from './fr-events-home.json'
+import pt from './pt-events-home.json'
 
 const ffEventsHome = {
     en,
     es,
+    fr,
+    pt,
 }
 
 export default ffEventsHome;
--- src/assets/data/sponsors/index.js
+++ src/assets/data/sponsors/index.js
@@ -1,9 +1,13 @@
 import en from './en-sponsors.json'
 import es from './es-sponsors.json'
+import fr from './fr-sponsors.json'
+import pt from './pt-sponsors.json'
 
 const ffsponsors = {
     en,
     es,
+    fr,
+    pt,
 }
 
 export default ffsponsors;
