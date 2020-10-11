65f1be408b7bc873ae6a78a2d31ccfe25f43e1ae
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-10-11 12:01:20 -0400

    Add vue-i18n configuration to application

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { createI18n } from 'vue-i18n'
+// To Get i18n values
+import { valuesI18n } from "./i18n";
+// createI18n: pass the values
+const i18n = createI18n(valuesI18n)
 
-const app = createApp(App).use(router);
+// Create the app
+const app = createApp(App);
 
+// Add uses
 app.use(router);
+app.use(i18n);
 
+// Mounted
 app.mount('#app');
