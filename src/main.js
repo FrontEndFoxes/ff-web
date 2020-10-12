5dee61942b3828e8da00651753280eb24f18661c
JasonChan <jasonnchann24@gmail.com>
2020-10-12 13:21:33 +0700

    add watcher for meta

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import { createI18n } from 'vue-i18n'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createI18n } from "vue-i18n";
 // To Get i18n values
 import { valuesI18n } from "./i18n";
 // createI18n: pass the values
-const i18n = createI18n(valuesI18n)
+const i18n = createI18n(valuesI18n);
 
 // Create the app
 const app = createApp(App);
@@ -15,4 +15,4 @@ app.use(router);
 app.use(i18n);
 
 // Mounted
-app.mount('#app');
+app.mount("#app");
