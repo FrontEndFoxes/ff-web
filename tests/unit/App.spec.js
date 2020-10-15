82e3f6b2d91c147c509d985826e1e360aa7587d7
albasilventefuentes <alba@square1.io>
2020-10-15 21:43:43 +0200

    Fix test unit errors due to router global plugin

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,18 +1,26 @@
 import { shallowMount } from '@vue/test-utils'
+import router from '@/router'
 
 // Component
 import App from '@/App.vue'
 
 describe('App.vue', () => {
     let wrapper
-    
+
     const mountFunction = options => {
         return shallowMount(App, {
+            global: {
+                plugins: [
+                    router
+                ]
+            },
             ...options,
         })
     }
 
-    beforeEach(() => {
+    beforeEach(async () => {
+        router.push('/')
+        await router.isReady()
         wrapper = mountFunction()
     })
 
