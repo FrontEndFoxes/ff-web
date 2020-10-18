c64f6d0ca459417c3c3c13c411e14b7a385fff55
Jaeriah Tay <jaeriahpasseri@gmail.com>
2020-10-18 13:57:37 -0700

    add sass loader, styling tweaks to about page, add event link

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,59 +1,59 @@
 <template>
-  <div class="pt-24 bg-blue-100 bg-gradient-to-b from-white pb-10">
+  <div class="about pt-24 bg-blue-100 bg-gradient-to-b from-white pb-10">
     <h1 class="about__title">{{ $t("about.aboutTitle") }}</h1>
-    <div class="w-full mb-4">
       <div
-        class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"
+        class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t mb-16"
       ></div>
-    </div>
-    <cld-image cloudName="front-end-foxes" publicId="sample" width="300" />
-    <div class="text-black mx-20 text-xl">
-      <p>
-        {{ $t("about.aboutPurpose") }}
-      </p>
-      <h2 class="my-4 text-2xl font-bold leading-tight">
-        {{ $t("about.activitiesSubtitle") }}
-      </h2>
-      <p>
-        {{ $t("about.activitiesDescription") }}
-      </p>
-      <h2 class="my-4 text-2xl font-bold leading-tight">
-        {{ $t("about.mentorsNoteSubtitle") }}
-      </h2>
+    <div class="about__content">
+        <cld-image cloudName="front-end-foxes" publicId="sample" width="300" />
+        <div class="text-black mx-20 text-xl">
+        <p>
+            {{ $t("about.aboutPurpose") }}
+        </p>
+        <h2 class="my-4 text-2xl font-bold leading-tight">
+            {{ $t("about.activitiesSubtitle") }}
+        </h2>
+        <p>
+            {{ $t("about.activitiesDescription") }}
+        </p>
+        <h2 class="my-4 text-2xl font-bold leading-tight">
+            {{ $t("about.mentorsNoteSubtitle") }}
+        </h2>
 
-      <p>
-        {{ $t("about.mentorsNote") }}
-      </p>
-      <h2 class="my-4 text-2xl font-bold leading-tight">
-        {{ $t("about.studentsNoteSubtitle") }}
-      </h2>
+        <p>
+            {{ $t("about.mentorsNote") }}
+        </p>
+        <h2 class="my-4 text-2xl font-bold leading-tight">
+            {{ $t("about.studentsNoteSubtitle") }}
+        </h2>
 
-      <p>
-        {{ $t("about.studentSentences.first") }}
-      </p>
-      <p class="py-5">"{{ $t("about.studentSentences.second") }}"</p>
-      <p>{{ $t("about.studentSentences.third") }} 🦊</p>
+        <p>
+            {{ $t("about.studentSentences.first") }}
+        </p>
+        <p class="py-5">"{{ $t("about.studentSentences.second") }}"</p>
+        <p>{{ $t("about.studentSentences.third") }} 🦊</p>
 
-      <h2 class="my-4 text-2xl font-bold leading-tight">
-        {{ $t("about.ffSponsoringSubtitle") }}
-      </h2>
-      <p>
-        {{ $t("about.ffSponsoringDescription") }}
-      </p>
-      <p class="my-4">{{ $t("about.ffSponsoringPots.platinum") }}</p>
-      <p class="my-4">{{ $t("about.ffSponsoringPots.gold") }}</p>
-      <p class="my-4">{{ $t("about.ffSponsoringPots.silver") }}</p>
-      <p class="my-4">{{ $t("about.ffSponsoringPots.softwareSponsor") }}</p>
-      <p class="my-4">
-        {{ $t("about.ffSponsoringPots.patreonSponsors") }}
-        <a
-          href="https://www.patreon.com/vuevixens"
-          target="_blank"
-          rel="noopener"
-          >Patreon</a
-        >
-        {{ $t("about.ffSponsoringPots.githubSponsors") }}
-      </p>
+        <h2 class="my-4 text-2xl font-bold leading-tight">
+            {{ $t("about.ffSponsoringSubtitle") }}
+        </h2>
+        <p>
+            {{ $t("about.ffSponsoringDescription") }}
+        </p>
+        <p class="my-4">{{ $t("about.ffSponsoringPots.platinum") }}</p>
+        <p class="my-4">{{ $t("about.ffSponsoringPots.gold") }}</p>
+        <p class="my-4">{{ $t("about.ffSponsoringPots.silver") }}</p>
+        <p class="my-4">{{ $t("about.ffSponsoringPots.softwareSponsor") }}</p>
+        <p class="my-4">
+            {{ $t("about.ffSponsoringPots.patreonSponsors") }}
+            <a
+            href="https://www.patreon.com/vuevixens"
+            target="_blank"
+            rel="noopener"
+            >Patreon</a
+            >
+            {{ $t("about.ffSponsoringPots.githubSponsors") }}
+        </p>
+        </div>
     </div>
   </div>
 </template>
@@ -65,8 +65,15 @@ export default defineComponent({
   name: "About",
 });
 </script>
-<style scoped>
-.about__title {
-  @apply w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800;
+<style lang="scss" scoped>
+.about {
+    &__title {
+        @apply w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800;
+    }
+
+    &__content {
+        @apply max-w-6xl mx-auto;
+    }
+
 }
 </style>
