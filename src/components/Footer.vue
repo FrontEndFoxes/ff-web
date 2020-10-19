084dc376f5a72d5181e2e22420aa313e16530a0a
Farhia <farhia.fcb@gmail.com>
2020-10-19 19:26:04 +0200

    Comment out megaphone icon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -34,7 +34,7 @@
                             <a class="uppercase inline-block py-2 px-4 text-white no-underline"
                                 href="https://www.frontendfoxes.org/announcements" aria-label="Announcements"
                                 target="_blank" rel="noopener">
-                                <i class="text-white fa fa-bullhorn fa-2x" aria-hidden="true"></i>
+                                <!-- <i class="text-white fa fa-bullhorn fa-2x" aria-hidden="true"></i> -->
                             </a>
                         </li>
                         <li class="mr-3">
