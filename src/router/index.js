bfc982db5e6f673669dc0584fe3c5d4fec69caaa
Jen Looper <jen.looper@gmail.com>
2020-10-30 17:54:18 -0400

    french translations, one error in header fixed

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
         path: "/",
         name: "Home/",
         component: Home,
-        meta: { title: "Front-end Foxes" }
+        meta: { title: "Front-End Foxes" }
     },
     {
         path: "/about/",
