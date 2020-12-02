27068ad4a9d211032121fb8d05666c63f846ea5d
mariajcb <bogomazm@gmail.com>
2020-12-02 16:25:03 -0700

    sponsorship links complete

--- src/views/About.vue
+++ src/views/About.vue
@@ -68,46 +68,59 @@
       </div>
     </div>
 
-    <div class="sponsors text-center">
+    <div class="sponsors">
         <div class="mx-40 my-10 pt-10">
-          <h2 class="my-4 text-3xl font-semibold">
+          <h2 class="my-4 text-3xl font-semibold text-center">
               {{ $t("about.ffSponsoringSubtitle") }}
           </h2>
-          <p class="px-2 lg:px-40 pt-4 leading-relaxed text-lg">
+          <p class="px-2 lg:px-32 pt-4 leading-relaxed text-lg text-center">
               {{ $t("about.ffSponsoringDescription") }}
           </p>
-            <div class="lg:flex px-40 pt-4">
-              <div class="lg:w-1/3">
-                  <img class="inline" src="@/assets/images/silver.png" :alt="$t('about.ffSponsoringPots.silverTitle')" />
-                  <h3 class="text-xl text-bold -mt-8">{{ $t("about.ffSponsoringPots.silverTitle") }}</h3>
-                  <p class="mt-3">{{ $t("about.ffSponsoringPots.silver") }}</p>
-              </div>
-              <div class="lg:w-1/3">
-                  <img class="inline" src="@/assets/images/gold.png" :alt="$t('about.ffSponsoringPots.goldTitle')" />
-                  <h3 class="text-xl text-bold -mt-8">{{ $t("about.ffSponsoringPots.goldTitle") }}</h3>
-                  <p class="mt-3">{{ $t("about.ffSponsoringPots.gold") }}</p>
-              </div>
-              <div class="lg:w-1/3">
-                  <img class="inline" src="@/assets/images/platinum.png" :alt="$t('about.ffSponsoringPots.platinum')" />
-                  <h3 class="text-xl text-bold -mt-8">{{ $t("about.ffSponsoringPots.platinumTitle") }}</h3>
-                  <p class="mt-3">{{ $t("about.ffSponsoringPots.platinum") }}</p>
-              </div>
-            </div>
 
-          <p class="my-4">{{ $t("about.ffSponsoringPots.softwareSponsor") }}</p>
-          <p class="my-4">
-              {{ $t("about.ffSponsoringPots.patreonSponsors") }}
+          <div class="lg:flex px-40 pt-4 text-center">
+            <div class="lg:w-1/3">
+                <img class="inline" src="@/assets/images/silver.png" :alt="$t('about.ffSponsoringPots.silverTitle')" />
+                <h3 class="text-xl text-bold -mt-8">{{ $t("about.ffSponsoringPots.silverTitle") }}</h3>
+                <p class="mt-3"><i>{{ $t("about.ffSponsoringPots.silver") }}</i></p>
+            </div>
+            <div class="lg:w-1/3">
+                <img class="inline" src="@/assets/images/gold.png" :alt="$t('about.ffSponsoringPots.goldTitle')" />
+                <h3 class="text-xl text-bold -mt-8">{{ $t("about.ffSponsoringPots.goldTitle") }}</h3>
+                <p class="mt-3"><i>{{ $t("about.ffSponsoringPots.gold") }}</i></p>
+            </div>
+            <div class="lg:w-1/3">
+                <img class="inline" src="@/assets/images/platinum.png" :alt="$t('about.ffSponsoringPots.platinum')" />
+                <h3 class="text-xl text-bold -mt-8">{{ $t("about.ffSponsoringPots.platinumTitle") }}</h3>
+                <p class="mt-3"><i>{{ $t("about.ffSponsoringPots.platinum") }}</i></p>
+            </div>
+          </div>
+
+          <p class="mt-20 my-4 text-xl">
+              <img class="inline mr-4 icon" src="@/assets/images/wrench.png" alt=""/>
+              <span class="sponsor-link">{{ $t("about.ffSponsoringPots.softwareTitle") }}</span>
+              {{ $t("about.ffSponsoringPots.softwareSponsor") }}
+          </p>
+          <p class="my-4 text-xl">
+              <img class="inline mr-4 icon" src="@/assets/images/patreon.png" alt=""/>
               <a
               href="https://www.patreon.com/vuevixens"
               target="_blank"
               rel="noopener"
-              >Patreon</a
-              >
+              class="sponsor-link"
+              >{{ $t("about.ffSponsoringPots.patreonTitle") }}</a>
+              {{ $t("about.ffSponsoringPots.patreonSponsors") }}
+          </p>
+          <p class="my-4 text-xl">
+              <img class="inline mr-4 icon" src="@/assets/images/github.png" alt=""/>
+              <a
+              href="https://github.com/sponsors/FrontEndFoxes"
+              target="_blank"
+              rel="noopener"
+              class="sponsor-link"
+              >{{ $t("about.ffSponsoringPots.githubTitle") }}</a>
               {{ $t("about.ffSponsoringPots.githubSponsors") }}
           </p>
         </div>
-
-
     </div>
   </div>
 </template>
@@ -148,5 +161,25 @@ export default defineComponent({
 
   .sponsors {
     background-color: #072D48;
+  }
+
+  .icon {
+    height: 24px;
+  }
+
+  .sponsor-link {
+    transition: background-size .4s ease;
+    background: linear-gradient(to bottom, transparent 62%, #C23A14 0) center center/0% 75% no-repeat;
+    padding: 0 6px 2px 6px;
+    cursor: pointer;
+    letter-spacing: .08rem;
+  }
+
+  .sponsor-link:hover {
+    background-size: 100% 100%;
+  }
+
+  .sponsor-link:active {
+    background-size: 80% 100%;
   }
 </style>
