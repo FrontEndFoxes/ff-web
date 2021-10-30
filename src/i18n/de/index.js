88c58a27890a40b00b46d72d079d080dace7ae4b
Andrea Busse <andrea.busse@codecentric.nl>
2021-10-30 20:12:52 +0200

    added german translation

---
+++ src/i18n/de/index.js
@@ -0,0 +1,19 @@
+import about from './de-about.json'
+import footer from './de-footer.json'
+import gallery from './de-gallery.json'
+import header from './de-header.json'
+import home from './de-home.json'
+import notfound from './de-notfound.json'
+import sponsors from './de-sponsors.json'
+import team from './de-team.json'
+
+export const de = {
+  header,
+  footer,
+  home,
+  about,
+  sponsors,
+  team,
+  gallery,
+  notfound
+}
